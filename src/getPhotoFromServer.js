c19f20272d852563d1d0aedaa1d8b737c265ea0a
Vitalii900 <melnikvitaliy30@gmail.com>
2022-12-19 23:35:57 +0200

    continue work

--- src/getPhotoFromServer.js
+++ src/getPhotoFromServer.js
@@ -1,10 +1,10 @@
 import axios from 'axios';
 
-export default async function getPhotoFromServer(value) {
+export default async function getPhotoFromServer(value, page) {
   const KEY = '32193446-673e16063f0204736e3ddb7cd';
   try {
     const response = await axios.get(
-      `https://pixabay.com/api/?key=${KEY}&q=${value}&image_type=photo&orientation=horizontal&safesearch=true`
+      `https://pixabay.com/api/?key=${KEY}&q=${value}&image_type=photo&orientation=horizontal&safesearch=true&per_page=40&page=${page}`
     );
     const arrayOfPhoto = response.data.hits;
     return arrayOfPhoto;
--- src/index.js
+++ src/index.js
@@ -1,16 +1,29 @@
-import axios from 'axios';
 import getPhotoFromServer from './getPhotoFromServer';
 
 const formRef = document.querySelector('#search-form');
+const galleryRef = document.querySelector('.gallery');
+const loadMoreButtonRaf = document.querySelector('.load-more');
 
 formRef.addEventListener('submit', searchPhoto);
+loadMoreButtonRaf.addEventListener('click', loadMore);
+
+let pageCounter = 1;
+let inputValue = '';
 
 async function searchPhoto(event) {
   event.preventDefault();
-  const inputValue = formRef.elements.searchQuery.value;
+  pageCounter = 1;
+  inputValue = event.target.elements.searchQuery.value;
+  const array = await getPhotoFromServer(inputValue, pageCounter);
+  addMurkupOnPage(array);
+  
+}
 
-  const array = await getPhotoFromServer(inputValue);
-  console.log(array);
+async function loadMore() {
+  pageCounter += 1;
+  const array = await getPhotoFromServer(inputValue, pageCounter);
+  addMurkupOnPage(array);
+  // console.log(array);
 }
 
 function addMurkupOnPage(arrayOfObjects) {
@@ -23,6 +36,29 @@ function addMurkupOnPage(arrayOfObjects) {
       views,
       comments,
       downloads,
-    }) => {}
-  );
+    }) => {
+      return `<div class="photo-card">
+              <a href="${largeImageURL}"> 
+                <img src="${webformatURL}" alt="${tags}" loading="lazy" />
+              </a>
+              <div class="info">
+                <p class="info-item">
+                  <b>Likes: </b>${likes}
+                </p>
+                <p class="info-item">
+                  <b>Views: </b>${views}
+                </p>
+                <p class="info-item">
+                  <b>Comments: </b>${comments}
+                </p>
+                <p class="info-item">
+                  <b>Downloads: </b>${downloads}
+                </p>
+              </div>
+      </div>`;
+      }
+    )
+    .join('');
+
+  galleryRef.insertAdjacentHTML('beforeend', murkupFromArray);
 }
