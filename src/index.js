cc62f0845fcd62493c737bc2d13cb909ed6bd0dd
Vitalii900 <melnikvitaliy30@gmail.com>
2022-12-19 22:17:07 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -5,16 +5,24 @@ const formRef = document.querySelector('#search-form');
 
 formRef.addEventListener('submit', searchPhoto);
 
-
 async function searchPhoto(event) {
-    event.preventDefault();
-    const inputValue = formRef.elements.searchQuery.value;
-    
-    const array = await getPhotoFromServer(inputValue);
-    console.log(array);
+  event.preventDefault();
+  const inputValue = formRef.elements.searchQuery.value;
 
+  const array = await getPhotoFromServer(inputValue);
+  console.log(array);
 }
 
 function addMurkupOnPage(arrayOfObjects) {
-    
+  const murkupFromArray = arrayOfObjects.map(
+    ({
+      webformatURL,
+      largeImageURL,
+      tags,
+      likes,
+      views,
+      comments,
+      downloads,
+    }) => {}
+  );
 }
